6a9c4f8e369d87997b41beb3b3d2ae0a497cc843
basilisk1869 <basilisk@yandex.ru>
2022-07-04 17:33:48 +0300

    feat: добавить аннотацию @AfterOrEqualsDate

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -5,6 +5,7 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import ru.yandex.practicum.filmorate.annotation.AfterOrEqualsDate;
 
 import javax.validation.constraints.*;
 import java.time.LocalDate;
@@ -22,20 +23,9 @@ public class Film extends Base {
     @Size(max = 200)
     private String description;
 
-    @NotNull
+    @AfterOrEqualsDate(value = "1895-12-28")
     private LocalDate releaseDate;
 
     @Positive
     private double duration;
-
-    @AssertTrue(message = "Дата релиза должна быть не раньше 28 декабря 1895 года")
-    boolean isReleaseDateNotLaterCinemaBirth() {
-        if (releaseDate == null) {
-            return true;
-        } else {
-            LocalDate cinemaBirth = LocalDate.of(1895, 12, 28);
-            return releaseDate.isAfter(cinemaBirth) || releaseDate.isEqual(cinemaBirth);
-        }
-    }
-
 }
